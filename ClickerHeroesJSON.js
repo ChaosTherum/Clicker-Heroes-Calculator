b93afe8a18b184096748f50fb294e447743717bb
ChaosTherum <chaostherum@gmail.com>
2014-12-30 14:13:02 -0500

    started with dreadknight
    Started adding his parameters

--- ClickerHeroesJSON.js
+++ ClickerHeroesJSON.js
@@ -2676,7 +2676,14 @@ chdata =
             "costFormula": "heroCostFormula1",
             "baseClickDamage": 0,
             "specialSkill": ""
+        },
+        "27": {
+        "baseGoldPerSecond": 0,
+            "specialSkillDescription": "",
+            "name": "Dread Knight",
+            
         }
+    
     },
     "zones": {
         "1": {
